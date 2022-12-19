76bebc49c7a2225899cd0b0d048f60884b5637cc
Nima Bazofti <nimabaz@yahoo.com>
2022-12-19 14:58:40 -0800

    video preview commit

---
+++ Web Fundamentals/JavaScript/Document Object Model/Button Clicker/script.js
@@ -0,0 +1,12 @@
+function changeText(element) {
+    if (element.innerText == "Login") {
+        element.innerText = "Logout";
+    }
+    else {
+        element.innerText = "Login"
+    }
+}
+
+function removeButton(element) {
+    element.remove();
+}
---
+++ Web Fundamentals/JavaScript/Document Object Model/Video Preview/script.js
@@ -0,0 +1,12 @@
+console.log("page loaded...");
+
+function playVid(element) {
+    element.play();
+    // console.log(element);
+}
+
+function pauseVid(element) {
+    element.pause();
+    // console.log(element);
+}
+
