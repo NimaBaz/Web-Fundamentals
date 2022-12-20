4074f7deb338762aa0adff7666d8e78f0ac54d8e
Nima Bazofti <nimabaz@yahoo.com>
2022-12-20 14:45:55 -0800

    Likes commit

---
+++ Web Fundamentals/JavaScript/Document Object Model/Likes/script.js
@@ -0,0 +1,6 @@
+function liked(id) {
+    document.querySelector(id).innerText++;
+}
+
+
+
---
+++ Web Fundamentals/JavaScript/Document Object Model/Making Connections/script.js
@@ -0,0 +1,19 @@
+// Creating a function to add item from list
+function addItem(id) {
+    
+}
+// Creating a function to remove item from list
+function removeItem(id) {
+    
+}
+
+
+function switchName(element) {
+    if (element.innerText == "Jane Doe") {
+        element.innerText = "Nima Bazofti";
+    }
+    else {
+        element.innerText = "Jane Doe"
+    }
+}
+
